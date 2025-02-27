814e448a3b58f3d62c797400c04d72b1441db806
nguyenthianhhuyen03@gmail.com <nguyenthianhhuyen03@gmail.com>
2025-02-27 10:10:39 +0700

    fix reset pass controller

--- src/main/java/vn/iotstar/ecoveggieapp/controllers/ResetPasswordController.java
+++ src/main/java/vn/iotstar/ecoveggieapp/controllers/ResetPasswordController.java
@@ -1,10 +1,13 @@
 package vn.iotstar.ecoveggieapp.controllers;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,21 +22,23 @@ public class ResetPasswordController {
 	UserService userService;
 	
 	@PostMapping("/user/resetpassword")
-	public ResponseEntity<String> resetPassword(@RequestParam("email") String email,
-												@RequestParam("newPassword") String newPassword) {
-		if (email.isEmpty() || newPassword.isEmpty()) {
-			return new ResponseEntity<>("Vui lòng điền đầy đủ thông tin!", HttpStatus.BAD_REQUEST);
-		}
+	public ResponseEntity<String> resetPassword(@RequestBody Map<String, String> payload) {
+	    String email = payload.get("email");
+	    String newPassword = payload.get("newPassword");
 
-		// Băm mật khẩu mới
-		String hashedPassword = BCrypt.hashpw(newPassword, BCrypt.gensalt());
+	    if (email == null || newPassword == null) {
+	        return new ResponseEntity<>("Vui lòng điền đầy đủ thông tin!", HttpStatus.BAD_REQUEST);
+	    }
 
-		// Cập nhật mật khẩu trong database
-		int result = userService.resetUserPassword(email, hashedPassword);
+	    // Băm mật khẩu mới
+	    String hashedPassword = BCrypt.hashpw(newPassword, BCrypt.gensalt());
 
-		if (result != 1) {
-			return new ResponseEntity<>("Không thể cập nhật mật khẩu, vui lòng thử lại!", HttpStatus.BAD_REQUEST);
-		}
-		return new ResponseEntity<>("success", HttpStatus.OK);
+	    // Cập nhật mật khẩu trong database
+	    int result = userService.resetUserPassword(email, hashedPassword);
+
+	    if (result != 1) {
+	        return new ResponseEntity<>("Không thể cập nhật mật khẩu, vui lòng thử lại!", HttpStatus.BAD_REQUEST);
+	    }
+	    return new ResponseEntity<>("success", HttpStatus.OK);
 	}
 }
